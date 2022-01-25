73a553bf2a4ed048690709cd38924fd372db3667
aria.ding <aria.ding@klook.com>
2022-01-25 10:44:37 +0800

    feat: init

---
+++ test.js
@@ -0,0 +1,6 @@
+export function loadWebAssembly(path) {
+  return fetch(path)                   // 加载文件        
+    .then(res => res.arrayBuffer())    // 转成 ArrayBuffer
+    .then(WebAssembly.instantiate)     // 编译 + 实例化
+    .then(mod => mod.instance)         // 提取生成都模块
+}
